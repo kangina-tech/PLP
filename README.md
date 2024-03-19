a13345c84f3e253a58022def482eda3b736a9ba8
Kangina <85179015+kangina-tech@users.noreply.github.com>
2024-03-19 17:56:47 +0300

    First python algorithm (simple)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PLP
+
+All the assignments under the PLP umbrella are stored here.
---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+location = input('Where are you from? ')
+
+print("Hello", name, "! You are", age, "and live in", location, ".")
